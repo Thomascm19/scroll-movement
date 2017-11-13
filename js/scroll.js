dfc2e18211eafda2041e0bf9e588ffe2e619340e
thomas.caycedo <thomascaycedo19@outlook.com>
2017-11-13 13:25:00 -0500

    Se le da movimiento a los articulos

--- js/scroll.js
+++ js/scroll.js
@@ -4,7 +4,9 @@ EL OBJETO CON LAS PROPIEDADDES DEL SCROLL
 
 var p = {
 
-
+	posicionScroll: 0,
+	articulos: document.querySelectorAll("#scroll article"),
+	cajaScroll: document.querySelector("#scroll"),
 }
 
 
@@ -14,15 +16,29 @@ EL OBJETO CON LOS METODOS DEL SCROLL
 =============================================*/
 
 var m = {
-	inicioScroll: function(){
+	inicioScroll: function() {
 
 		document.addEventListener("scroll", m.efectoParallax)
-	
-	},
-		efectoParallax: function(){
 
-			console.log(window.pageYOffset)
+	},
+	efectoParallax: function() {
+
+		p.posicionScroll = window.pageYOffset;
+
+		if (p.posicionScroll > p.cajaScroll.offsetTop-200) {
+
+			for (var i = 0; i < p.articulos.length; i++) {
+				p.articulos[i].style.marginLeft = "0%";
+			}
+		} else {
+
+			for (var i = 0; i < p.articulos.length; i++) {
+
+				p.articulos[i].style.marginLeft = p.posicionScroll / 25 - 100 + "%";
+			}
+
 		}
+	}
 
 }
 m.inicioScroll();
