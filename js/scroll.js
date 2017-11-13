8257a88564c4086d34076c1ef79648d85c8d4900
thomas.caycedo <thomascaycedo19@outlook.com>
2017-11-13 13:48:07 -0500

    Se le da movimiento a los articulos corrigiendo errores

--- js/scroll.js
+++ js/scroll.js
@@ -21,20 +21,24 @@ var m = {
 		document.addEventListener("scroll", m.efectoParallax)
 
 	},
+
 	efectoParallax: function() {
 
 		p.posicionScroll = window.pageYOffset;
 
-		if (p.posicionScroll > p.cajaScroll.offsetTop-200) {
+		if (p.posicionScroll > p.cajaScroll.offsetbottom - 200) {
 
 			for (var i = 0; i < p.articulos.length; i++) {
+
 				p.articulos[i].style.marginLeft = "0%";
+
 			}
+
 		} else {
 
 			for (var i = 0; i < p.articulos.length; i++) {
 
-				p.articulos[i].style.marginLeft = p.posicionScroll / 25 - 100 + "%";
+				p.articulos[i].style.marginLeft = p.posicionScroll / 10 - 100 + "%";
 			}
 
 		}
